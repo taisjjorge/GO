13dd2e41a653c56f3941edf4c805de4371704eb3
taisjjorge <taisjjorge@gmail.com>
2022-08-31 21:17:44 -0300

    add: Middleware and solving CORS problem

--- projeto-orm/routes/routes.go
+++ projeto-orm/routes/routes.go
@@ -5,16 +5,19 @@ import (
 	"net/http"
 
 	"api-rest.com/controllers"
+	"api-rest.com/middleware"
+	"github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
 )
 
 func HandleRequest(){
 	r := mux.NewRouter()
+	r.Use(middleware.ContentTypeMiddleware)
 	r.HandleFunc("/", controllers.Home)
 	r.HandleFunc("/api/personalidades", controllers.TodasPersonalidades).Methods("Get")
 	r.HandleFunc("/api/personalidades/{id}", controllers.RetornaPersonalidade).Methods("Get")
 	r.HandleFunc("/api/personalidades", controllers.CriaPersonalidade).Methods("Post")
 	r.HandleFunc("/api/personalidades/{id}", controllers.DeletaPersonalidade).Methods("Delete")
 	r.HandleFunc("/api/personalidades/{id}", controllers.AtualizaPersonalidade).Methods("Put")
-	log.Fatal(http.ListenAndServe(":8000", r))
+	log.Fatal(http.ListenAndServe(":8000", handlers.CORS(handlers.AllowedOrigins([]string{"*"}))(r)))
 }
