f29adbdf17b37852bf21114ae2503ee8c5cdcb12
taisjjorge <taisjjorge@gmail.com>
2022-09-01 23:08:30 -0300

    feat: Learning tests in Go using testify

--- api-go-gin/models/aluno.go
+++ api-go-gin/models/aluno.go
@@ -1,12 +1,20 @@
 package models
 
-import "gorm.io/gorm"
+import (
+	"gopkg.in/validator.v2"
+	"gorm.io/gorm"
+)
 
 type Aluno struct {
-	gorm.Model //https://gorm.io/docs/models.html#Embedded-Struct
-	Nome string `json:"nome"`
-	CPF  string `json:"cpf"`
-	RG   string `json:"rg"`
+	gorm.Model        //https://gorm.io/docs/models.html#Embedded-Struct
+	Nome string `json:"nome" validate:"nonzero"`
+	RG   string `json:"rg" validate:"len=9, regexp=^[0-9]*$"`
+	CPF  string `json:"cpf" validate:"len=11, regexp=^[0-9]*$"`
 }
 
-//var Alunos []Aluno
+func ValidadorAlunos(aluno *Aluno) error {
+	if err := validator.Validate(aluno); err != nil{
+		return err
+	}
+	return nil
+}
