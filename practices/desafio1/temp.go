8aaf856c933a3a3262a0681af442b5d669f4a146
tais.amanda <tais.amanda@g.globo>
2023-03-22 10:50:49 -0300

    add: Resolução de exercício conversão termométricas

---
+++ practices/desafio1/temp.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+const ebulicaoK = 373.0
+
+func main() {
+
+	tempK := ebulicaoK
+	tempC := tempK - 273.0
+	
+	fmt.Printf("A temperatura de ebulicao da água em K = %g e a temperatura de ebulicao da água em C = %g.", tempK, tempC)
+}
